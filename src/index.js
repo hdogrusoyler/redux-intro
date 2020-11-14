f48311b4df59a8633d5c0344f4ace4948f4271dd
HUS <HUS@LAPTOP>
2020-11-14 13:37:47 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import configureStore from './redux/reducers/configureStore';
 const store = configureStore();
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store="store">
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
